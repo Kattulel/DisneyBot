0af98ea7a2494ecc77946d6c6c6580f2f237990e
Gabriel <61303132+Kattulel@users.noreply.github.com>
2020-03-11 23:07:18 -0300

    Add files via upload

---
+++ data/listeners.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+
+
+class Listeners(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    #@commands.Cog.listener()
+    #async def on_message(self, member):
+
+
